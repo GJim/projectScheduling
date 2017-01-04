696d5343143f32200042cd6bf36cc806a4ee2776
unknown <>
2017-01-04 22:09:33 +0800

    v1

---
+++ routes/index.js
@@ -0,0 +1,421 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/schedule', function(req, res) {
+	let classrooms = JSON.parse(req.query.classrooms);
+	let projects = JSON.parse(req.query.projects);
+	let locks = JSON.parse(req.query.locks);
+	let acceptions = JSON.parse(req.query.acceptions);
+	let ranks = JSON.parse(req.query.ranks);
+	//a blending method for genetic algorithm
+	//take 2 group for evolving
+	//take 10 best one from 2 group
+	let ga1 = new geneticAlgorithm();
+	let ga2 = new geneticAlgorithm();
+	ga1.initial(classrooms, projects, locks, acceptions, ranks);
+	ga2.initial(classrooms, projects, locks, acceptions, ranks);
+	/*the group with 2000 generations and no mutation*/
+	for(let i=0; i<2000; i++) {
+		ga1.crossover();
+		ga1.selections();
+	}
+	/*the group with 2000 generations and 199 times mutation*/
+	for(let i=0; i<2000; i++) {
+		ga2.crossover();
+		ga2.selections();
+		((i-10)%10 == 1) && ga2.mutation();
+	}
+	let g1 = ga1.getGeneration();
+	let g2 = ga2.getGeneration();
+	let rs = g1.concat(g2);
+	rs = blending(rs);
+	let stages = clean(rs.stages);
+	res.status(200).json({score: rs.score, stages: stages, process: rs.process});
+});
+
+function geneticAlgorithm() {
+	this.classroom = [];
+	this.project = [];
+	this.lock = [];
+	this.acception = [];
+	this.rank = [];
+	this.generations = [];
+	this.stageStruture = null;
+	this.initial = function(classroom, project, lock, acception, rank) {
+		this.classroom = classroom;
+		this.project = project;
+		this.lock = lock;
+		this.acception = acception;
+		this.rank = rank;
+		//initial 10 generations
+		this.stageStruture = fixedProject(this.project, this.classroom);
+		for(let i=0; i<10; i++){
+			let g = putTeacher(this.stageStruture, this.acception);
+			let f = fitness(g, this.acception, this.project, this.lock);
+			this.generations.push({stages: g, score: f.score, process: f.process});
+		}
+	};
+	this.crossover = function() {
+		//initialize parent generation value and avoid overriding
+		//original generation value
+		//father mean the best one
+		//mother mean the second
+		let father = this.generations[this.generations.length-1].stages.slice();
+		let tmp = this.generations[this.generations.length-2].stages.slice();
+		let mother = tmp[0].concat(tmp[1], tmp[2], tmp[3]);
+		//take stage structure and a half of project teachers'
+		//combination from father
+		//take a half of project teachers' combination from mother
+		let r = [0, 1, 2, 3];
+		r = randomArray(r);
+		let c1 = r.pop();
+		let c2 = r.pop();
+		for(let i=0; i<father[c1].length; i++) {
+			for(let j=0; j<mother.length; j++) {
+				if(father[c1][i].project.name == mother[j].project.name) {
+					father[c1][i].teachers = mother[j].teachers;
+				}
+			}
+		}
+		for(let i=0; i<father[c2].length; i++) {
+			for(let j=0; j<mother.length; j++) {
+				if(father[c2][i].project.name == mother[j].project.name) {
+					father[c2][i].teachers = mother[j].teachers;
+				}
+			}
+		}
+		let f = fitness(father, this.acception, this.project, this.lock);
+		this.generations.push({stages: father, score: f.score, process: f.process});
+	};
+	this.mutation =  function() {
+		this.stageStruture = fixedProject(this.project, this.classroom);
+		for(let i=0; i<1; i++){
+			let g = putTeacher(this.stageStruture, this.acception);
+			let f = fitness(g, this.acception, this.project, this.lock);
+			this.generations.push({stages: g, score: f.score, process: f.process});
+			this.generations.sort(function(a, b) {
+				return a.score - b.score;
+			});
+			this.generations.pop();
+		}
+	};
+	this.selections = function() {
+		//sort the array by generation's fitness ascending
+		this.generations.sort(function(a, b) {
+			return a.score - b.score;
+		});
+		//abandon the worse one
+		this.generations.pop();
+	};
+	this.getGeneration = function() {
+		//sort the array by generation's fitness decending
+		this.generations.sort(function(a, b) {
+			return b.score - a.score;
+		});
+		return this.generations;
+	}
+}
+
+/*fixed the project into available classroom*/
+function fixedProject(project, classroom) {
+	//the project display in the specific time, and the place call stage
+	//divide projects into four array accroding to time segment
+	let stages1 = [];
+	let stages2 = [];
+	let stages3 = [];
+	let stages4 = [];
+	//clone the project array to avoid editing original variable
+	let pj = project.slice();
+	//random project array
+	pj = randomArray(pj);
+	//fixed the project into classroom
+	for(let i=0; i<classroom.length/4; i++) {
+		if(classroom[i].available) {
+			let stage = {
+				project: pj.shift(),
+				teachers: []
+			};
+			stages1.push(stage);
+		}
+	}
+	for(let i=classroom.length/4; i<(classroom.length/4)*2; i++) {
+		if(classroom[i].available) {
+			let stage = {
+				project: pj.shift(),
+				teachers: []
+			};
+			stages2.push(stage);
+		}
+	}
+	for(let i=(classroom.length/4)*2; i<(classroom.length/4)*3; i++) {
+		if(classroom[i].available) {
+			let stage = {
+				project: pj.shift(),
+				teachers: []
+			};
+			stages3.push(stage);
+		}
+	}
+	for(let i=(classroom.length/4)*3; i<classroom.length; i++) {
+		if(classroom[i].available) {
+			let stage = {
+				project: pj.shift(),
+				teachers: []
+			};
+			stages4.push(stage);
+		}
+	}
+	return [stages1, stages2, stages3, stages4];
+}
+
+/*put the teacher into stage*/
+function putTeacher(generation, acception) {
+	let stages1 = generation[0];
+	let stages2 = generation[1];
+	let stages3 = generation[2];
+	let stages4 = generation[3];
+	//put teacher into ever stage
+	//set a condition to avoid the reviewed teacher is
+	//project's professor
+	//use one teachers' array in one time segment avoid the same
+	//teacher showing up in same project or in same time segment
+	let ts = randomArray(acception.slice());
+	for(let i=0; i<stages1.length; i++) {
+		let c = 0;
+		stages1[i].teachers = [];
+		while(c < 3) {
+			if(stages1[i].project.professor == ts[0].name) {
+				stages1[i].teachers.push(ts[1]);
+				ts.splice(1, 1);
+			} else {
+				stages1[i].teachers.push(ts.shift());
+			}
+			c++;
+		}
+	}
+	ts = randomArray(acception.slice());
+	for(let i=0; i<stages2.length; i++) {
+		let c = 0;
+		stages2[i].teachers = [];
+		while(c < 3) {
+			if(stages2[i].project.professor == ts[0].name) {
+				stages2[i].teachers.push(ts[1]);
+				ts.splice(1, 1);
+			} else {
+				stages2[i].teachers.push(ts.shift());
+			}
+			c++;
+		}
+	}
+	ts = randomArray(acception.slice());
+	for(let i=0; i<stages3.length; i++) {
+		let c = 0;
+		stages3[i].teachers = [];
+		while(c < 3) {
+			if(stages3[i].project.professor == ts[0].name) {
+				stages3[i].teachers.push(ts[1]);
+				ts.splice(1, 1);
+			} else {
+				stages3[i].teachers.push(ts.shift());
+			}
+			c++;
+		}
+	}
+	ts = randomArray(acception.slice());
+	for(let i=0; i<stages4.length; i++) {
+		let c = 0;
+		stages4[i].teachers = [];
+		while(c < 3) {
+			if(stages4[i].project.professor == ts[0].name) {
+				stages4[i].teachers.push(ts[1]);
+				ts.splice(1, 1);
+			} else {
+				stages4[i].teachers.push(ts.shift());
+			}
+			c++;
+		}
+	}
+	return [stages1, stages2, stages3, stages4];
+}
+
+/*calculate the fitness of the generation*/
+function fitness(generation, acception, project, lock) {
+	let stages1 = generation[0];
+	let stages2 = generation[1];
+	let stages3 = generation[2];
+	let stages4 = generation[3];
+	let score = 0;
+	let process = [];
+	//initialize the rank of leading and reviewing quantity
+	let leadingRank = [];
+	let reviewingRank = [];
+	for(let i=0; i<acception.length; i++) {
+		leadingRank.push({name: acception[i].name, quantity: 0});
+		reviewingRank.push({name: acception[i].name, quantity: 0});
+	}
+	for(let i=0; i<project.length; i++) {
+		for(let j=0; j<leadingRank.length; j++) {
+			leadingRank[j].quantity = (project[i].professor == leadingRank[j].name) ? leadingRank[j].quantity+1 : leadingRank[j].quantity;
+		}
+	}
+	//regulation1: give the 50 points of penalty when distributed
+	//teacher in unavailable time segment
+	//regulation2: give the 50 points of penalty when distributed
+	//teachers having heart lock show up in same project
+	//regulation3: give 20 point of bonus when the sorting rank
+	//of the teacher's reviewing (project) quantity is same 
+	//with the rank of the teacher's leading (project) quantity
+	//and vice versa
+	//regulation4: give the 5 points of bonus when distributed
+	//teacher is the project's first reviewed teacher
+	for(let i=0; i<stages1.length; i++) {
+		for(let j=0; j<3; j++) {
+			score = stages1[i].teachers[j].first ? score : score-50;
+			if(!stages1[i].teachers[j].first) process.push("first time segment "+stages1[i].teachers[j].name+" isn't available");
+			for(let k=0; k<lock.length; k++) {
+				if(stages1[i].teachers[j].name == lock[k].a) {
+					for(let l=j+1; l<3; l++) {
+						score = (stages1[i].teachers[l].name == lock[k].b) ? score-50 : score;
+						if(stages1[i].teachers[l].name == lock[k].b) process.push("first time segment "+stages1[i].teachers[j].name+" show up with "+stages1[i].teachers[l].name);
+					}
+				}
+			}
+			for(let k=0; k<reviewingRank.length; k++) {
+				reviewingRank[k].quantity = (stages1[i].teachers[j].name == reviewingRank[k].name) ? reviewingRank[k].quantity+1 : reviewingRank[k].quantity;
+			}
+			if(stages1[i].project.teacher.indexOf(stages1[i].teachers[j].name) > -1) score += 5;
+		}
+	}
+	for(let i=0; i<stages2.length; i++) {
+		for(let j=0; j<3; j++) {
+			score = stages2[i].teachers[j].second ? score : score-50;
+			if(!stages2[i].teachers[j].second) process.push("second time segment "+stages2[i].teachers[j].name+" isn't available");
+			for(let k=0; k<lock.length; k++) {
+				if(stages2[i].teachers[j].name == lock[k].a) {
+					for(let l=j+1; l<3; l++) {
+						score = (stages2[i].teachers[l].name == lock[k].b) ? score-50 : score;
+						if(stages2[i].teachers[l].name == lock[k].b) process.push("second time segment "+stages2[i].teachers[j].name+" show up with "+stages2[i].teachers[l].name);
+					}
+				}
+			}
+			for(let k=0; k<reviewingRank.length; k++) {
+				reviewingRank[k].quantity = (stages2[i].teachers[j].name == reviewingRank[k].name) ? reviewingRank[k].quantity+1 : reviewingRank[k].quantity;
+			}
+			if(stages2[i].project.teacher.indexOf(stages2[i].teachers[j].name) > -1) score += 5;
+		}
+	}
+	for(let i=0; i<stages3.length; i++) {
+		for(let j=0; j<3; j++) {
+			score = stages3[i].teachers[j].third ? score : score-50;
+			if(!stages3[i].teachers[j].third) process.push("third time segment "+stages3[i].teachers[j].name+" isn't available");
+			for(let k=0; k<lock.length; k++) {
+				if(stages3[i].teachers[j].name == lock[k].a) {
+					for(let l=j+1; l<3; l++) {
+						score = (stages3[i].teachers[l].name == lock[k].b) ? score-50 : score;
+						if(stages3[i].teachers[l].name == lock[k].b) process.push("third time segment "+stages3[i].teachers[j].name+" show up with "+stages3[i].teachers[l].name);
+					}
+				}
+			}
+			for(let k=0; k<reviewingRank.length; k++) {
+				reviewingRank[k].quantity = (stages3[i].teachers[j].name == reviewingRank[k].name) ? reviewingRank[k].quantity+1 : reviewingRank[k].quantity;
+			}
+			if(stages3[i].project.teacher.indexOf(stages3[i].teachers[j].name) > -1) score += 5;
+		}
+	}
+	for(let i=0; i<stages4.length; i++) {
+		for(let j=0; j<3; j++) {
+			score = stages4[i].teachers[j].fourth ? score : score-50;
+			if(!stages4[i].teachers[j].fourth) process.push("fourth time segment "+stages4[i].teachers[j].name+" isn't available");
+			for(let k=0; k<lock.length; k++) {
+				if(stages4[i].teachers[j].name == lock[k].a) {
+					for(let l=j+1; l<3; l++) {
+						score = (stages4[i].teachers[l].name == lock[k].b) ? score-50 : score;
+						if(stages4[i].teachers[l].name == lock[k].b) process.push("fourth time segment "+stages4[i].teachers[j].name+" show up with "+stages4[i].teachers[l].name);
+					}
+				}
+			}
+			for(let k=0; k<reviewingRank.length; k++) {
+				reviewingRank[k].quantity = (stages4[i].teachers[j].name == reviewingRank[k].name) ? reviewingRank[k].quantity+1 : reviewingRank[k].quantity;
+			}
+			if(stages4[i].project.teacher.indexOf(stages4[i].teachers[j].name) > -1) score += 5;
+		}
+	}
+	reviewingRank.sort(function(a, b) {
+		return a.quantity - b.quantity;
+	});
+	leadingRank.sort(function(a, b) {
+		return a.quantity - b.quantity;
+	});
+	for(let i=0; i<reviewingRank.length; i++) {
+		if(leadingRank[i].quantity > 0) {
+			score = (reviewingRank[i].name == leadingRank[i].name) ? score+10 : score-10;
+			if(reviewingRank[i].name != leadingRank[i].name) process.push(reviewingRank[i].name+" ranking error!")
+		}
+	}
+	return {score: score, process: process};
+}
+
+//blending the different group and select the best one
+function blending(array) {
+	array.sort(function(a, b) {
+		return b.score - a.score;
+	});
+	return array[0];
+}
+
+//clean the redundant data
+function clean(data) {
+	let result = [];
+	for(let i=0; i<data[0].length; i++) {
+		let t = [];
+		for(let j=0; j<data[0][i].teachers.length; j++) {
+			t.push(data[0][i].teachers[j].name);
+		}
+		result.push({project: data[0][i].project.name, teachers: t})
+	}
+	for(let i=0; i<data[1].length; i++) {
+		let t = [];
+		for(let j=0; j<data[1][i].teachers.length; j++) {
+			t.push(data[1][i].teachers[j].name);
+		}
+		result.push({project: data[1][i].project.name, teachers: t})
+	}
+	for(let i=0; i<data[2].length; i++) {
+		let t = [];
+		for(let j=0; j<data[2][i].teachers.length; j++) {
+			t.push(data[2][i].teachers[j].name);
+		}
+		result.push({project: data[2][i].project.name, teachers: t})
+	}
+	for(let i=0; i<data[3].length; i++) {
+		let t = [];
+		for(let j=0; j<data[3][i].teachers.length; j++) {
+			t.push(data[3][i].teachers[j].name);
+		}
+		result.push({project: data[3][i].project.name, teachers: t})
+	}
+	return result;
+}
+
+//the array random method from
+//https://github.com/coolaj86/knuth-shuffle
+function randomArray(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+module.exports = router;
---
+++ views/js/index.js
@@ -0,0 +1,248 @@
+var classrooms = [];
+
+//project information(project's name, project's professor, project's first reviewed teacher)
+var projects = [];
+
+//teacher heart locked relationship
+var locks = [];
+
+//teacher's available time
+var acceptions = [];
+
+var tmp = [];
+
+//teacher's wished reviewing project rank
+var ranks = [];
+function setClassroom() {
+	$("#ct").show();
+	$(".ca").show();
+	let rooms = $("#cInput").val().split(',');
+	let times = ["13:10-14:00", "14:10-15:00", "15:10-16:00", "16:10-17:00"];
+	$("#cth").html("<th>#</th>");
+	$("#cStage1").html("<th>"+times[0]+"</th>");
+	$("#cStage2").html("<th>"+times[1]+"</th>");
+	$("#cStage3").html("<th>"+times[2]+"</th>");
+	$("#cStage4").html("<th>"+times[3]+"</th>");
+	for(let i=0; i<rooms.length; i++) {
+		$("#cth").append("<th>"+rooms[i]+"</th>");
+		$("#cStage1").append('<td class="cStage" no="'+parseInt(i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+		$("#cStage2").append('<td class="cStage" no="'+parseInt(rooms.length+i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+		$("#cStage3").append('<td class="cStage" no="'+parseInt(rooms.length*2+i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+		$("#cStage4").append('<td class="cStage" no="'+parseInt(rooms.length*3+i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+	}
+	classrooms = [];
+	for(let i=0; i<rooms.length*4; i++) {
+		classrooms.push({room: rooms[i%rooms.length], time: times[parseInt(i/rooms.length)], available: true});
+	}
+}
+
+function toggleClassroom() {
+	let no = $(this).attr( "no" );
+	classrooms[no].available = !classrooms[no].available;
+	$(this).html(classrooms[no].available ? '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>')
+}
+
+function finishClassroom() {
+	$('#ss').tab('show');
+}
+
+function setProfessor() {
+	$("#tt").show();
+	$(".ta").show();
+	let professors = $("#tInput").val().split(',');
+	let times = ["13:10-14:00", "14:10-15:00", "15:10-16:00", "16:10-17:00"];
+	$("#tth").html("<th>#</th>");
+	$("#tStage1").html("<th>"+times[0]+"</th>");
+	$("#tStage2").html("<th>"+times[1]+"</th>");
+	$("#tStage3").html("<th>"+times[2]+"</th>");
+	$("#tStage4").html("<th>"+times[3]+"</th>");
+	for(let i=0; i<professors.length; i++) {
+		$("#tth").append("<th>"+professors[i]+"</th>");
+		$("#tStage1").append('<td class="tStage" no="'+parseInt(i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+		$("#tStage2").append('<td class="tStage" no="'+parseInt(professors.length+i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+		$("#tStage3").append('<td class="tStage" no="'+parseInt(professors.length*2+i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+		$("#tStage4").append('<td class="tStage" no="'+parseInt(professors.length*3+i)+'"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>');
+	}
+	acceptions = [];
+	for(let i=0; i<professors.length; i++) {
+		acceptions.push({name: professors[i%professors.length], first: true, second: true, third: true, fourth: true});
+	}
+}
+
+function toggleProfessor() {
+	let no = $(this).attr( "no" );
+	if(parseInt(no/acceptions.length) == 0) {
+		acceptions[no%acceptions.length].first = !acceptions[no%acceptions.length].first;
+		$(this).html(acceptions[no%acceptions.length].first ? '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+	} else if(parseInt(no/acceptions.length) == 1) {
+		acceptions[no%acceptions.length].second = !acceptions[no%acceptions.length].second;
+		$(this).html(acceptions[no%acceptions.length].second ? '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+	} else if(parseInt(no/acceptions.length) == 2) {
+		acceptions[no%acceptions.length].third = !acceptions[no%acceptions.length].third;
+		$(this).html(acceptions[no%acceptions.length].third ? '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+	} else {
+		acceptions[no%acceptions.length].fourth = !acceptions[no%acceptions.length].fourth;
+		$(this).html(acceptions[no%acceptions.length].fourth ? '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+	}
+}
+
+function finishProfessor() {
+	$('#ts').tab('show');
+}
+
+function setProject() {
+	$("#pt").show();
+	$(".pa").show();
+	let professors = $("#tInput").val().split(',');
+	let topics = $("#pInput").val().split(',');
+	console.log(professors);
+	console.log(topics);
+	$("#pth").html("<th>#</th>");
+	for(let i=0; i<topics.length; i++) {
+		$("#pth").append("<th>"+topics[i]+"</th>")
+	}
+	$("#pt tbody").html('');
+	for(let i=0; i<professors.length; i++) {
+		$("#pt tbody").append('<tr id="pStage'+i+'"><th>'+professors[i]+'</th></tr>');
+		for(let j=0; j<topics.length; j++) {
+			$("#pStage"+i).append('<td class="pStage" no="'+parseInt(i*topics.length+j)+'"></td>');
+			tmp.push(0);
+		}
+	}
+}
+
+function toggleProject() {
+	let no = $(this).attr( "no" );
+	tmp[no] = (tmp[no] === 1) ? 2 : 1;
+	$(this).html((tmp[no] === 1) ? '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+}
+
+function cancelProject() {
+	let no = $(this).attr( "no" );
+	tmp[no] = 0;
+	$(this).html('');
+}
+
+function finishProject() {
+	let professors = $("#tInput").val().split(',');
+	let topics = $("#pInput").val().split(',');
+	projects = [];
+	for(let i=0; i<topics.length; i++) {
+		projects.push({name: topics[i], professor: "", teacher: []});
+	}
+	for(let i=0; i<tmp.length; i++) {
+		if(tmp[i] === 1) {
+			projects[i%topics.length].teacher.push(professors[parseInt(i/topics.length)]);
+		} if(tmp[i] === 2) {
+			projects[i%topics.length].professor = professors[parseInt(i/topics.length)];
+		}
+	}
+	$('#fs').tab('show');
+}
+
+function setLock() {
+	$("#lt").show();
+	$(".la").show();
+	let professors = $("#tInput").val().split(',');
+	let number = $("#lInput").val();
+	let selector = "<select>";
+	for(let i=0; i<professors.length; i++) {
+		selector += '<option value="'+professors[i]+'">'+professors[i]+'</option>';
+	}
+	selector += "</select>"
+	$("#lt tbody").html('');
+	for(let i=0; i<parseInt(number); i++) {
+		$("#lt tbody").append('<tr><td id="l'+i+'1">'+selector+'</td><td id="l'+i+'2">'+selector+'</td></tr>');
+	}
+}
+
+function finishLock() {
+	let number = $("#lInput").val();
+	locks = [];
+	for(let i=0; i<parseInt(number); i++) {
+		let pa = $("#l"+i+"1 select").val();
+		let pb = $("#l"+i+"2 select").val();
+		locks.push({a: pa, b: pb});
+	}
+	$('#rs').tab('show');
+}
+
+function getResult() {
+	$.get("/schedule", 
+		{
+			"classrooms": JSON.stringify(classrooms),
+			"projects": JSON.stringify(projects),
+			"locks": JSON.stringify(locks),
+			"acceptions": JSON.stringify(acceptions),
+			"ranks": JSON.stringify(ranks)
+		},
+		function(data, status){
+			let rooms = $("#cInput").val().split(',');
+			let times = ["13:10-14:00", "14:10-15:00", "15:10-16:00", "16:10-17:00"];
+	    $("#score").html(data.score);
+	    $("#rth").html("<th>#</th>");
+			for(let i=0; i<rooms.length; i++) {
+				$("#rth").append("<th>"+rooms[i]+"</th>");
+			}
+			$("#rStage1").html("<th>"+times[0]+"</th>");
+			$("#rStage2").html("<th>"+times[1]+"</th>");
+			$("#rStage3").html("<th>"+times[2]+"</th>");
+			$("#rStage4").html("<th>"+times[3]+"</th>");
+			var c = 0;
+	    for(let i=0; i<classrooms.length; i++) {
+	    	if(classrooms[i].available) {
+	    		let p = "<b>專題題目:</b> "+data.stages[c].project;
+          let pj = JSON.parse(JSON.stringify(projects));
+	    		pj = pj.filter(p => p.name == data.stages[c].project);
+	    		let sty1 = (pj[0].teacher.indexOf(data.stages[c].teachers[0]))>-1 ? 'color:blue;' : '';
+	    		let sty2 = (pj[0].teacher.indexOf(data.stages[c].teachers[1]))>-1 ? 'color:blue;' : '';
+	    		let sty3 = (pj[0].teacher.indexOf(data.stages[c].teachers[2]))>-1 ? 'color:blue;' : '';
+	    		let t = "<br/><b>評審老師:</b><span style='" + sty1 + "'>"
+	    				  +data.stages[c].teachers[0]
+	    				  +"</span>,<span style='" + sty2 + "'>"
+	    				  +data.stages[c].teachers[1]
+	    				  +"</span>,<span style='" + sty3 + "'>"
+	    				  +data.stages[c].teachers[2] +"</span>";
+	    		$( "#rStage" + parseInt(i/4+1) ).append('<td>'+p+t+'</td>');
+	    		c++;
+	    	} else {
+	    		$( "#rStage" + parseInt(i/4+1) ).append('<td></td>');
+	    	}
+	    }
+      let professors = $("#tInput").val().split(',');
+      let pNumber = [];
+      for(let i=0; i<data.stages.length; i++) {
+        for(let j=0; j<3; j++) {
+          pNumber[professors.indexOf(data.stages[i].teachers[j])] = pNumber[professors.indexOf(data.stages[i].teachers[j])] ? pNumber[professors.indexOf(data.stages[i].teachers[j])]+1 : 1;
+        }
+      }
+      $("#nth").html('');
+      $("#ntd").html('');
+      for(let i=0; i<professors.length; i++) {
+        $("#nth").append("<th>"+professors[i]+"</th>");
+        let num = pNumber[i] || 0;
+        $("#ntd").append("<td>"+num+"</td>");
+      }
+      $("#record").html('');
+      for(let i=0; i<data.process.length; i++) {
+        $("#record").append("<p>"+data.process[i]+"</p>");
+      }
+		}
+	);
+}
+
+$(document).ready(function(){
+	$("#createCR").click(setClassroom);
+	$("#ct").on("click", ".cStage", toggleClassroom);
+	$("#nextCR").click(finishClassroom);
+	$("#createPF").click(setProfessor);
+	$("#tt").on("click", ".tStage", toggleProfessor);
+	$("#nextPF").click(finishProfessor);
+	$("#createPJ").click(setProject);
+	$("#pt").on("click", ".pStage", toggleProject);
+	$("#pt").on("dblclick", ".pStage", cancelProject);
+	$("#nextPJ").click(finishProject);
+	$("#createLC").click(setLock);
+	$("#nextLC").click(finishLock);
+	$("#createRS").click(getResult);
+});
